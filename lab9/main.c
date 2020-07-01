cfaa85c0378522541c9c38f6897789ffd78fbb33
Pedro Ripper <pedros.ripper@globo.com>
2020-07-01 12:57:41 -0300

    lab 9 question 3 w error

--- lab9/main.c
+++ lab9/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h> 
 #include <string.h>
 #include <stdlib.h> 
+#include <dirent.h>
 
 #define MAX_TEXT 50
 
@@ -61,11 +62,15 @@ void question_one(){
 Questao 2
 Exibir os atributos de um dos arquivos criados.
 */
-void readFile(char *fileName){
+int readFile(char *fileName){
 	char text[MAX_TEXT];
 	FILE *readFile = fopen(fileName, "r");
+	if(readFile == NULL){
+		return 1;
+	}
 	fscanf(readFile, "%[^\n]", text);
    	printf("%s\n", text );
+   	return 0;
 }
 
 void question_two(){
@@ -75,11 +80,49 @@ void question_two(){
 	changeDirectory("../..");
 }
 
+/*
+Questao 3
+ Buscar um dado arquivo a partir do diretório “so”
+ ler e imprimir o conteúdo do arquivo encontrado
+*/
+int searchFile(char *fileName){
+	struct dirent *de;
+	DIR *directory = opendir("."); 
+	if (directory == NULL) { 
+        return 0; 
+    } 
+    if(readFile(fileName) == 1){
+		while ((de = readdir(directory)) != NULL){ 
+			if(strcmp(de->d_name,".") == 0|| strcmp(de->d_name,"..") == 0 || strcmp(de->d_name,"..") == 0){
+				printf("cai\n");
+				continue;
+			}
+			printf("%s\n", de->d_name); 
+			changeDirectory(de->d_name);
+			if(searchFile(fileName) == 0){
+				exit(0);
+			}
+		}
+	closedir(directory); 
+    } else {
+    	return 0;
+    }
+      
+    return 0;
+
+}
+
+
+void question_three(){
+	changeDirectory("so");
+	searchFile("arqb.txt");
+}
 
 
 int main(){
 	// question_one();
-	question_two();
+	// question_two();
+	question_three();
 
 	return 0;
 
